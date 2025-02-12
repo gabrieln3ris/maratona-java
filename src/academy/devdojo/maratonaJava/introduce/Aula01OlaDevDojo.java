4f6cc7332817ae2ebecd2414e2f4335f5d823454
Gabriel Neris <gabrielneriscontato@gmail.com>
2025-02-12 17:04:29 -0300

    Arquivos Iniciais de estudos

---
+++ src/academy/devdojo/maratonaJava/introduce/Aula01OlaDevDojo.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonaJava.introduce;
+
+public class Aula01OlaDevDojo
+{
+    public static void main (String[] args  ){
+        System.out.println("COMECO DO GRIND");
+
+    }
+
+}
---
+++ src/academy/devdojo/maratonaJava/introduce/Aula02TiposPrimitivos.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonaJava.introduce;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+
+        // int, double , float, char, byte, short, long, boolean
+
+        int age = 10;
+        System.out.println(age);
+
+    }
+}
